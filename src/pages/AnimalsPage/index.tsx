96b1c2c224233a22e9be0b5d47d569c7fb2f4bfd
rodrigo <rodrigocsilva63@gmail.com>
2023-10-18 23:31:27 -0300

    animal page section one

--- src/pages/AnimalsPage/index.tsx
+++ src/pages/AnimalsPage/index.tsx
@@ -1,12 +1,59 @@
-import * as React from 'react';
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import './style.css'
 
-interface IAnimalsProps {
-}
-
-export function Animals () {
+export function Animals() {
   return (
     <div>
-      Animals
+      <Header></Header>
+      <div className='select-section-container-one'>
+        <div className='animal-banner'>
+          <img src='image_4.png' />
+        </div>
+        <div className='select-section'>
+          <div className='select-section-title'>
+            <h1>Bixinhos</h1>
+            <img src='Vector_1.png' />
+          </div>
+          <div className='select-section-container'>
+            <div className='select-section-container-filter'>
+              <p>Filtrar por</p>
+              <select></select>
+              <div className='value-filter'>
+                <p>Valor</p>
+                <select></select>
+              </div>
+            </div>
+            <div className='select-section-container-content'>
+              <div className='content'>
+                <span>MEOW - GATO (F) - 6 ANOS</span>
+                <img src='cross.png' />
+              </div>
+              <div className='content'>
+                <span>MEOW - GATO (F) - 6 ANOS</span>
+                <img src='cross.png' />
+              </div>
+              <div className='content'>
+                <span>MEOW - GATO (F) - 6 ANOS</span>
+                <img src='cross.png' />
+              </div>
+              <div className='content'>
+                <span>MEOW - GATO (F) - 6 ANOS</span>
+                <img src='cross.png' />
+              </div>
+              <div className='content'>
+                <span>MEOW - GATO (F) - 6 ANOS</span>
+                <img src='cross.png' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='container-two'>
+
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
--- src/pages/homePage/index.tsx
+++ src/pages/homePage/index.tsx
@@ -3,7 +3,6 @@ import Header from '../../components/Header';
 import '../../assets/globalStyle.css'
 import './style.css'
 import { Link } from 'react-router-dom';
-import { Schedule } from '../SchedulePage';
 
 export function Home() {
   return (
