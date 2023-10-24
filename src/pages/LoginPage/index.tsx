96f74031e818a90c42bcf75200d5868e4e2fd4e5
rodrigo <rodrigocsilva63@gmail.com>
2023-10-24 19:37:47 -0300

    login page

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import './style.css';
 
+export function Login() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-interface ILoginProps {
-}
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`)
+  }
 
-export function Login () {
   return (
     <div>
-      Login
+      <Header></Header>
+      <div className='container-one-login'>
+        <h1>Login</h1>
+        <div className="login-form">
+          <form>
+            <label>Email</label>
+            <input type="text" onChange={(e) => setEmail(e.target.value)} />
+            <label>Senha</label>
+            <input type="password" onChange={(e) => setPassword(e.target.value)} />
+          </form>
+        </div>
+        <div className="login-form-buttons">
+          <button className="signup-button">Cadastrar</button>
+          <button className="signin-button">Entrar <img src="../../../public/paw_button.png" /></button>
+        </div>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
